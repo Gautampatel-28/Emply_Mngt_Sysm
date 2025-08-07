f91f99d6d5604f6b51aa1c0852cd891cf770125f
Gautampatel-28 <gautampatel7233@gmail.com>
2025-08-07 16:56:16 +0530

    app.jsx function created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
-
 import "./App.css";
 
 function App() {
-
-  return <>Frontend</>;
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-blue-600 text-white text-3xl font-bold">
+        Tailwind is Working 🎉
+      </div>
+    </>
+  );
 }
 
 export default App;
