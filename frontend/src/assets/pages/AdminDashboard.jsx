e72d38e99a92871ec20787a9154a0263551bbdb9
Gautampatel-28 <gautampatel7233@gmail.com>
2025-08-11 22:09:30 +0530

    jsx created

--- frontend/src/assets/pages/AdminDashboard.jsx
+++ frontend/src/assets/pages/AdminDashboard.jsx
@@ -1,11 +1,22 @@
-import { useAuth } from "../context/AuthContext";
+import "../styles/adminDashboard.css";
+// import { useAuth } from "../context/AuthContext";
+import Navbar from "../components/dashboard/Navbar";
+import AdminSidebar from "../components/dashboard/AdminSidebar";
+// import AdminSummary from "../components/dashboard/AdminSummary";
+import { Outlet } from "react-router-dom";
 
 const AdminDashboard = () => {
-  const { user } = useAuth();
-
+  // const { user } = useAuth();
   return (
     <>
-      <div>AdminDashboard {user && user.name}</div>
+      <div className="admin-dashboard">
+        <AdminSidebar />
+        <div className="admin-dashboard-content">
+          <Navbar />
+          {/* <AdminSummary /> */}
+          <Outlet />
+        </div>
+      </div>
     </>
   );
 };
