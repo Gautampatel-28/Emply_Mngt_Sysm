9e350bc1cd0af202065388edd6d0711b4ef1875c
Gautampatel-28 <gautampatel7233@gmail.com>
2025-08-11 12:02:50 +0530

    AuthContext added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "./index.css"
 import App from './App.jsx'
+import "./index.css"  
+import AuthContext from './assets/context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <AuthContext>
     <App />
-  </StrictMode>,
-)
+  </AuthContext>,
+) 
