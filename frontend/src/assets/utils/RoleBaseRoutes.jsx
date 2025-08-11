f00582b7d42b4c6aecea8836021a1d040e1a2fcb
Gautampatel-28 <gautampatel7233@gmail.com>
2025-08-11 22:16:29 +0530

    folder added

---
+++ frontend/src/assets/utils/RoleBaseRoutes.jsx
@@ -0,0 +1,21 @@
+import { Navigate } from "react-router-dom"
+import { useAuth } from "../context/AuthContext"
+
+const RoleBaseRoutes = ({children, requiredRole}) => {
+    const {user, loading} = useAuth()
+
+
+    if(loading){
+        <div>Loading ....</div>
+    }
+
+    if(!requiredRole.includes(user.role)){
+        <Navigate to="/unauthorized"  />
+    }
+
+    
+    return user ? children : <Navigate to="/admin-dashboard" />
+
+}
+
+export default RoleBaseRoutes
