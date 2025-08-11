4dc94df776f415f9de4f4f4d8643ca2d5b260614
Gautampatel-28 <gautampatel7233@gmail.com>
2025-08-11 12:05:20 +0530

    AuthRouter added

--- sever/index.js
+++ sever/index.js
@@ -1,12 +1,15 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv";
+import authRouter from "./routes/auth.js"
+import connectToDatabase from "./db/db.js"
 
 const app = express()
+connectToDatabase()
+dotenv.config();
 app.use(cors())
 app.use(express.json())
-dotenv.config();
-
+app.use("/api/auth", authRouter)
 
 app.listen(process.env.PORT, () => {
     console.log(`server is running on port ${process.env.PORT}`)
