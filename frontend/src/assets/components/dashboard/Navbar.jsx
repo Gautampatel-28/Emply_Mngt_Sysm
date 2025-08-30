14576d1a53293658ba23afc453ae14d2e6961bc7
Gautampatel-28 <gautampatel7233@gmail.com>
2025-08-30 14:11:38 +0530

    logout added

--- frontend/src/assets/components/dashboard/Navbar.jsx
+++ frontend/src/assets/components/dashboard/Navbar.jsx
@@ -1,14 +1,23 @@
 import { useAuth } from "../../context/AuthContext";
 import "../../styles/navbar.css"
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+  
+
+  const handleLogout = () => {
+    logout(); 
+    navigate("/login"); 
+    console.log("User logged out successfully")
+  };
 
   return (
     <>
       <div className="navbar">
         <p className="navbar-welcome">Welcome, {user.name}</p>
-        <button className="navbar-logout">Logout</button>
+        <button className="navbar-logout" onClick={handleLogout}>Logout</button>
       </div>
     </>
   );
