dadd2e905933f48b95f27b419ee66f013720e5f7
Gautampatel-28 <gautampatel7233@gmail.com>
2025-08-30 14:10:35 +0530

    add/view/edit jsx added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,10 @@ import RoleBaseRoutes from "./assets/utils/RoleBaseRoutes";
 import DepartmentList from "./assets/components/departments/DepartmentList";
 import AddDepartment from "./assets/components/departments/AddDepartment";
 import EditDepartment from "./assets/components/departments/EditDepartment";
+import List from "./assets/components/employee/List";
+import Add from "./assets/components/employee/Add";
+import View from "./assets/components/employee/View";
+import Edit from "./assets/components/employee/Edit";
 
 function App() {
   return (
@@ -31,6 +35,10 @@ function App() {
             <Route path="/admin-dashboard/departments" element={<DepartmentList />}></Route>
             <Route path="/admin-dashboard/add-department" element={<AddDepartment />}></Route>
             <Route path="/admin-dashboard/department/:id" element={<EditDepartment />}></Route>
+            <Route path="/admin-dashboard/employees" element={<List />}></Route>
+            <Route path="/admin-dashboard/add-employee" element={<Add />}></Route>
+            <Route path="/admin-dashboard/employees/:id" element={<View />}></Route>
+            <Route path="/admin-dashboard/employees/edit/:id" element={<Edit />}></Route>
             
 
           </Route>
