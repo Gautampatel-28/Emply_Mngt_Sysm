a50adf4450a350bc8cf0bc2ce82b49d5560633bb
Gautampatel-28 <gautampatel7233@gmail.com>
2025-08-30 14:20:03 +0530

    img_uploads

--- sever/index.js
+++ sever/index.js
@@ -4,14 +4,18 @@ import dotenv from "dotenv";
 import authRouter from "./routes/auth.js"
 import connectToDatabase from "./db/db.js"
 import departmentRouter from "./routes/department.js"
+import employeeRouter from "./routes/employee.js"
 
 const app = express()
 connectToDatabase()
 dotenv.config();
 app.use(cors())
 app.use(express.json())
+// app.use(express.static('public/uploads'))
+app.use("/uploads", express.static("/uploads"));
 app.use("/api/auth", authRouter)
 app.use("/api/department", departmentRouter)
+app.use("/api/employee", employeeRouter)
 
 app.listen(process.env.PORT, () => {
     console.log(`server is running on port ${process.env.PORT}`)
