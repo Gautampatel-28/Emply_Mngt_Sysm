075158b2f6aec65cb937f202dd5d87d463aeeaa3
Gautampatel-28 <gautampatel7233@gmail.com>
2025-08-30 14:39:03 +0530

    emp_router added

---
+++ sever/routes/employee.js
@@ -0,0 +1,14 @@
+import express from "express";
+import authMiddleware from "../middleware/authMiddleware.js";
+import { addEmployee, upload, getEmployees, getEmployee, updateEmployee } from "../controller/employeeController.js";
+
+
+const router = express.Router();
+
+router.get("/", authMiddleware, getEmployees);
+router.post("/add", authMiddleware, upload.single("profileImage"), addEmployee);
+router.get("/:id", authMiddleware, getEmployee);
+router.put("/:id", authMiddleware, updateEmployee);
+// router.delete("/:id", authMiddleware, deleteDepartment);
+
+export default router;
