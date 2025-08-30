a105cda0149b828f99520fbe93f1871ff9fcc853
Gautampatel-28 <gautampatel7233@gmail.com>
2025-08-30 14:21:53 +0530

    dept_router added

--- sever/routes/department.js
+++ sever/routes/department.js
@@ -1,6 +1,6 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
-import { addDepartment, getDepartments, editDepartment, updateDepartment } from "../controller/departmentController.js";
+import { addDepartment, getDepartments, editDepartment, updateDepartment, deleteDepartment } from "../controller/departmentController.js";
 
 const router = express.Router();
 
@@ -8,6 +8,7 @@ router.get("/", authMiddleware, getDepartments);
 router.post("/add", authMiddleware, addDepartment);
 router.get("/:id", authMiddleware, editDepartment);
 router.put("/:id", authMiddleware, updateDepartment);
+router.delete("/:id", authMiddleware, deleteDepartment);
 
 
 export default router;
