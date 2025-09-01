46f232b816871b2032520ee41d7ac3d7cfca8c41
Gautampatel-28 <gautampatel7233@gmail.com>
2025-09-01 12:59:41 +0530

    logout code chgs

--- frontend/src/assets/components/dashboard/Navbar.jsx
+++ frontend/src/assets/components/dashboard/Navbar.jsx
@@ -1,23 +1,14 @@
 import { useAuth } from "../../context/AuthContext";
 import "../../styles/navbar.css"
-import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const { user, logout } = useAuth();
-  const navigate = useNavigate();
-  
-
-  const handleLogout = () => {
-    logout(); 
-    navigate("/login"); 
-    console.log("User logged out successfully")
-  };
 
   return (
     <>
       <div className="navbar">
         <p className="navbar-welcome">Welcome, {user.name}</p>
-        <button className="navbar-logout" onClick={handleLogout}>Logout</button>
+        <button className="navbar-logout" onClick={logout}>Logout</button>
       </div>
     </>
   );
