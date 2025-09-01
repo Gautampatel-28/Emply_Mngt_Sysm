65248000eaebb8b0aae4788904473a88751c0aac
Gautampatel-28 <gautampatel7233@gmail.com>
2025-09-01 11:10:21 +0530

    routers  added

--- sever/index.js
+++ sever/index.js
@@ -5,17 +5,18 @@ import authRouter from "./routes/auth.js"
 import connectToDatabase from "./db/db.js"
 import departmentRouter from "./routes/department.js"
 import employeeRouter from "./routes/employee.js"
+import salaryRouter from "./routes/salary.js"
 
 const app = express()
 connectToDatabase()
 dotenv.config();
 app.use(cors())
 app.use(express.json())
-// app.use(express.static('public/uploads'))
 app.use("/uploads", express.static("/uploads"));
 app.use("/api/auth", authRouter)
 app.use("/api/department", departmentRouter)
 app.use("/api/employee", employeeRouter)
+app.use("/api/salary", salaryRouter)
 
 app.listen(process.env.PORT, () => {
     console.log(`server is running on port ${process.env.PORT}`)
